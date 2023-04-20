2ddabb03209f5bcca3afa18fc1746f8d7347fb5f
carlsylvan <carlsylvan@gmail.com>
2023-04-20 14:51:41 +0200

    cmsclient

--- src/clients/cmsClient.ts
+++ src/clients/cmsClient.ts
@@ -7,28 +7,91 @@ const client = axios.create({
 export class CmsClient {
     constructor () {};
 
+    //Televisions
     public async getTelevisions(): Promise<any> {
         const response = await client.get('/televisions');
         return response.data;
     }
-
     public async getTelevision(id: number): Promise<any> {
         const response = await client.get(`/televisions/${id}`);
         return response.data;
     }
-
     public async deleteTelevision(id: number): Promise<any> {
         const response = await client.delete(`/televisions/${id}`);
         return response.data;
     }
-
     public async postTelevision(): Promise<any> {
         const response = await client.post('/televisions');
         return response.data;
     }
-
     public async updateTelevision(id: number): Promise<any> {
         const response = await client.put(`/televisions/${id}`);
+        return response.data;
+    }
+
+    //Audio_devices
+    public async getAudio_devices(): Promise<any> {
+        const response = await client.get('/audio_devices');
+        return response.data;
+    }
+    public async getAudio_device(id: number): Promise<any> {
+        const response = await client.get(`/audio_devices/${id}`);
+        return response.data;
+    }
+    public async deleteAudio_device(id: number): Promise<any> {
+        const response = await client.delete(`/audio_devices/${id}`);
+        return response.data;
+    }
+    public async postAudio_device(): Promise<any> {
+        const response = await client.post('/audio_devices');
+        return response.data;
+    }
+    public async updateAudio_device(id: number): Promise<any> {
+        const response = await client.put(`/audio_devices/${id}`);
+        return response.data;
+    }
+
+    //Computers
+    public async getComputers(): Promise<any> {
+        const response = await client.get('/computers');
+        return response.data;
+    }
+    public async getComputer(id: number): Promise<any> {
+        const response = await client.get(`/computers/${id}`);
+        return response.data;
+    }
+    public async deleteComputer(id: number): Promise<any> {
+        const response = await client.delete(`/computers/${id}`);
+        return response.data;
+    }
+    public async postComputer(): Promise<any> {
+        const response = await client.post('/computers');
+        return response.data;
+    }
+    public async updateComputer(id: number): Promise<any> {
+        const response = await client.put(`/computers/${id}`);
+        return response.data;
+    }
+
+    //Mobiles
+    public async getMobiles(): Promise<any> {
+        const response = await client.get('/mobiles');
+        return response.data;
+    }
+    public async getMobile(id: number): Promise<any> {
+        const response = await client.get(`/mobiles/${id}`);
+        return response.data;
+    }
+    public async deleteMobile(id: number): Promise<any> {
+        const response = await client.delete(`/mobiles/${id}`);
+        return response.data;
+    }
+    public async postMobile(): Promise<any> {
+        const response = await client.post('/mobiles');
+        return response.data;
+    }
+    public async updateMobile(id: number): Promise<any> {
+        const response = await client.put(`/mobiles/${id}`);
         return response.data;
     }
 }
