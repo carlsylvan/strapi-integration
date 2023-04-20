e4e4bfbf8f4b81b68d8f0884490a426bc69d8470
carlsylvan <carlsylvan@gmail.com>
2023-04-20 14:59:18 +0200

    audio devices router fix

--- src/routes/audio_devicesRouter.ts
+++ src/routes/audio_devicesRouter.ts
@@ -5,9 +5,9 @@ import { Audio_devicesController } from "../controllers/audio_devicesController"
 const client = new CmsClient();
 const controller = new Audio_devicesController(client);
 
-export const televisionsRouter = express.Router();
+export const audio_devicesRouter = express.Router();
 
-televisionsRouter.get('/', controller.getAudio_devices.bind(controller))
+audio_devicesRouter.get('/', controller.getAudio_devices.bind(controller))
                  .get('/:id', controller.getAudio_device.bind(controller))
                  .delete('/:id', controller.deleteAudio_device.bind(controller))
                  .post('/', controller.postAudio_device.bind(controller))
