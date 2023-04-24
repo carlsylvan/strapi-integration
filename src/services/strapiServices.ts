4a59ea14180364fdf51238736dc1b35f41545cf9
carlsylvan <carlsylvan@gmail.com>
2023-04-24 13:10:39 +0200

    removed service

--- src/services/strapiServices.ts
+++
@@ -1,9 +0,0 @@
-// import axios from "axios";
-// import { ITelevision } from "../models/ITelevision";
-
-// const URL = "http://localhost:1337/api";
-
-// export const getTelevisions = async () => {
-//     let response = await axios.get<ITelevision[]>(`${URL}/televisions`);
-//     return response.data;
-// }
